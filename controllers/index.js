b8270b1acace8ccd9f47e61ebe0c0b76be088885
JosePortillo <jose.portillo.samayoa@gmail.com>
2023-04-27 15:09:01 -0600

    feat: :sparkles: adding HTTP methods for Users

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ module.exports.Controllers = {
 
     getUsers: async(req,res)=>{
         try {
-            const users = await sequelize.query('SELECT * FROM User', { type: sequelize.QueryTypes.SELECT });
+            const users = await sequelize.query(`select * from User where state = ${true} `, { type: sequelize.QueryTypes.SELECT });
             res.json(users)
         } catch (error) {
             console.log('An error occur getting users '+ error);
@@ -14,23 +14,55 @@ module.exports.Controllers = {
     getUserById: async(req,res)=>{
         try {
             const iduser = Number(req.params.id)
-            const user = await sequelize.query(`select * from User where id_user = ${iduser}`, {type: sequelize.QueryTypes.SELECT})
+            const user = await sequelize.query(`select * from User where id_user = ${iduser} and state = ${true}`, {type: sequelize.QueryTypes.SELECT})
+            if(!user.length > 0) return res.json({message: `User with id: ${iduser} doesn't exist into the database`})
+
             res.json(user)
         } catch (error) {
             throw new Error(error)
         }
     },
-    CreateUser: async(req,res)=>{
+    createUser: async(req,res)=>{
         const {name, email, password} = req.body
         try {
             let id_counter = await sequelize.query('select count(*) from User', { type: sequelize.QueryTypes.SELECT})
             let count = Object.values(id_counter[0])[0]+1
-            await sequelize.query(`insert into User(id_user,name,email,password,state) values(${count},'${name}','${email}','${password}', ${true});`, { type: sequelize.QueryTypes.INSERT });
-            res.json({message: 'User created successfully'})
+            await sequelize.query(`insert into User(id_user,name,email,password,state) 
+                                    values(${count},'${name}','${email}','${password}', ${true});`, { type: sequelize.QueryTypes.INSERT });
+            res.json({message: 'User was created successfully'})
         } catch (error) {
             console.log('An error occur creating user ' + error);
             throw new Error(error)
         }
         
+    },
+    updateUserById: async(req,res)=>{
+        const {name, email, password} = req.body
+        const id_user = Number(req.params.id)
+        
+        try {
+            const user = await sequelize.query(`select * from User where id_user = ${id_user}`,{type: sequelize.QueryTypes.SELECT})
+            if(!user.length > 0) return res.json({message: `The user with id ${id_user} doesn't exist into the database`})
+
+            await sequelize.query(`update User set name = '${name}', email = '${email}', password = '${password}'
+                                    where id_user = ${id_user};`,{type: sequelize.QueryTypes.UPDATE})
+            res.json({message: 'User was updated successfully'})
+        } catch (error) {
+            throw new Error(error)
+        }
+    },
+    deleteUserById: async(req,res) => {
+        try {
+            const id_user = Number(req.params.id)
+            const user = await sequelize.query(`select id_user from User where id_user = ${id_user};`,{type: sequelize.QueryTypes.SELECT})
+            if(!user.length > 0) return res.json({message: `The user with id ${id_user} doesn't exist into the database`})
+
+            await sequelize.query(`update User set state = ${false} 
+                                    where id_user = ${id_user};`,{type: sequelize.QueryTypes.UPDATE})
+            res.json({message: `User with id: ${id_user} was deleted`})
+        } catch (error) {
+            throw new Error(error)
+        }
     }
+
 }
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,17 @@ const router = express.Router()
 
 const { Controllers } = require('../controllers')
 
-
+//methods for User
 router.get('/users', Controllers.getUsers)
 router.get('/user/:id', Controllers.getUserById)
-router.post('/createUser', Controllers.CreateUser)
+router.post('/createUser', Controllers.createUser)
+router.put('/updateUser/:id', Controllers.updateUserById)
+router.delete('/deleteUser/:id',Controllers.deleteUserById)
+
+
+//methods for Account
+
+//Methods for Operation
+
 
 module.exports = router;
